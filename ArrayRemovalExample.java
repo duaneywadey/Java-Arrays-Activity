aaa29f0b4ab8c3a498e45dc6599d2c7c12398590
duaneywadey <idrdequito.cvt@eac.edu.ph>
2023-10-10 12:14:22 +0800

    add codes

---
+++ ArrayRemovalExample.java
@@ -0,0 +1,48 @@
+public class ArrayRemovalExample {
+    public static void main(String[] args) {
+        // Create an array
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        // Print the original array
+        System.out.println("Original Array:");
+        printArray(numbers);
+
+        // Remove the element at index 2
+        int indexToRemove = 2;
+        numbers = removeArrayElement(numbers, indexToRemove);
+
+        // Print the updated array
+        System.out.println("Updated Array:");
+        printArray(numbers);
+    }
+
+    public static int[] removeArrayElement(int[] array, int index) {
+        // Check if the index is valid
+        if (index >= 0 && index < array.length) {
+            // Create a new array with size one less than the original array
+            int[] newArray = new int[array.length - 1];
+
+            // Copy the elements from the original array to the new array
+            for (int i = 0, j = 0; i < array.length; i++) {
+                // Skip the element at the given index
+                if (i == index) {
+                    continue;
+                }
+                newArray[j] = array[i];
+                j++;
+            }
+
+            return newArray;
+        } else {
+            System.out.println("Invalid index!");
+            return array;
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ ArrayUpdateExample.java
@@ -0,0 +1,39 @@
+public class ArrayUpdateExample {
+    public static void main(String[] args) {
+        // Create an array
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        // Print the original array
+        System.out.println("Original Array:");
+        printArray(numbers);
+
+        // Update the element at index 2
+        int indexToUpdate = 2;
+        int newValue = 10;
+        updateArrayElement(numbers, indexToUpdate, newValue);
+
+        // Print the updated array
+        System.out.println("Updated Array:");
+        printArray(numbers);
+    }
+
+    public static void updateArrayElement(int[] array, int index, int newValue) {
+        // Check if the index is valid
+        if (index >= 0 && index < array.length) {
+            // Update the element at the given index
+            array[index] = newValue;
+        } else {
+            System.out.println("Invalid index!");
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+}
+
+
+
